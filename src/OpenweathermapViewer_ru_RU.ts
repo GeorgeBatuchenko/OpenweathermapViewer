11d43b4f8449218c0f5252ef4d2814001c81c5ea
George Batuychenko <georg.batuchenko@gmail.com>
2021-08-10 15:36:48 +0700

    Create basic qml  application

---
+++ src/OpenweathermapViewer_ru_RU.ts
@@ -0,0 +1,38 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ru_RU">
+<context>
+    <name>CitiesPage.ui</name>
+    <message>
+        <location filename="pages/cities/CitiesPage.ui.qml" line="8"/>
+        <source>Choose city</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>HomePage.ui</name>
+    <message>
+        <location filename="pages/home/HomePage.ui.qml" line="8"/>
+        <source>OpenWheatherMap Viewer</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="pages/home/HomePage.ui.qml" line="11"/>
+        <source>You are on the home page.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="MainWindow.qml" line="9"/>
+        <source>OpenWheatherMap Viewer</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="MainWindow.qml" line="42"/>
+        <source>Choose city</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
---
+++ src/pages/cities/CitiesPage.ui.qml
@@ -0,0 +1,10 @@
+import QtQuick 2.12
+import QtQuick.Controls 2.5
+
+Page {
+    width: 600
+    height: 400
+
+    title: qsTr("Choose city")
+
+}
