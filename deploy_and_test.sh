5266df652d202cf8c9c336df9a7ad589298b65a0
George Batuychenko <georg.batuchenko@gmail.com>
2021-08-24 17:04:37 +0700

    Update readme and fix some grammatical mistakes,

--- deploy_and_test.sh
+++ deploy_and_test.sh
@@ -18,9 +18,9 @@ if test -f /var/lock/OpenweathermapViwer/owapideploy.lock ; then
 		-h | --help)\
 			echo Usage: &&\
 			echo deploy_and_test [option] &&\
-			echo --upgrade-package - rebuild packege and update test container with removing all dependancies of old version and installing new  &&\
+			echo --upgrade-package - rebuild packege and update test container with removing all dependencies of old version and installing new  &&\
 			echo --build-package - only rebuild package &&\
-			echo --reinstall-package - reinstall deb package without apgrade dependancies &&\
+			echo --reinstall-package - reinstall deb package without apgrade dependencies &&\
 			echo No options - simply start test container  && exit 0;;
 		*) docker run --rm -t -i -v $PROJECT_FULL_PATH:/root/OpenweathermapViewer -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=unix$DISPLAY owapi-test;;
 	esac
